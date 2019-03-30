fd5d14f599a26bf38049a824d1f3e1ac9335618f
Víctor Romário <victor.romario.seriado@gmail.com>
2019-03-30 18:29:09 -0300

    Primeira versão do projeto com cambio de moedas

---
+++ cambio.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+url = "http://data.fixer.io/api/latest?access_key=5d0eb200bb9fb42872430c207728160f"
+print("Acessando base de dados...")
+response = requests.get(url)
+print(response)
+if response.status_code == 200:
+    print("Conseguiu acessar base de dados!")
+    print("Buscando informações das moedas...")
+    dados = response.json()
+    euro_real = dados['rates']['BRL'] / dados['rates']['EUR']
+    dollar_real = dados['rates']['BRL'] / dados['rates']['USD']
+    btc_real = dados['rates']['BRL'] / dados['rates']['BTC']
+    print("%.2f" % euro_real)
+    print("%.2f" % dollar_real)
+    print("%.2f" % btc_real)
+else:
+    print("Site com problemas!")
