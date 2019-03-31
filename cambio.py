34419a078331d4b7a241f30e5cf48a7212eefb84
Víctor Romário <victor.romario.seriado@gmail.com>
2019-03-31 16:38:24 -0300

    Adicionando nova coluna para data

--- cambio.py
+++ cambio.py
@@ -12,7 +12,8 @@ if response.status_code == 200:
     print("Buscando informações das moedas...")
     dados = response.json()
     day = dados['date']
-    print("Acessando dados do dia %s/%s/%s" % (day[8:], day[5:7], day[0:4]))
+    day = day[8:10]+"/"+day[5:7]+"/"+day[0:4]
+    print("Acessando dados do dia %s" % day) 
     euro_real = round(dados['rates']['BRL'] / dados['rates']['EUR'], 2)
     dollar_real = round(dados['rates']['BRL'] / dados['rates']['USD'], 2)
     btc_real = round(dados['rates']['BRL'] / dados['rates']['BTC'], 2)
@@ -20,8 +21,8 @@ if response.status_code == 200:
     print("%.2f" % dollar_real)
     print("%.2f" % btc_real)
     print("Gerando o arquivo CSV...")
-    df = pd.DataFrame({'Moedas': ['Euro', 'Dollar', 'Bitcoin'], 'Valores':[euro_real, dollar_real, btc_real]})
-    df.to_csv("valores.csv", index=False, sep=";")
+    df = pd.DataFrame({'Moedas': ['Euro', 'Dollar', 'Bitcoin'], 'Valores':[euro_real, dollar_real, btc_real], 'Dia de Acesso':[day, '', '']})
+    df.to_csv("valores.csv", index=False, sep=";", decimal=",")
     print("Arquivo gerado e exportado com sucesso para a pasta do projeto!")
 else:
     print("Site com problemas!")
